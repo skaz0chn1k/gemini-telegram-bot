2180a9294d1a9546b8ea4ce9f653c2cf4ce90b96
skaz0chn1k <1@mail66.org>
2024-07-12 14:16:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -198,12 +198,12 @@ async def gemini(bot,message,m):
         user_id = quote(str(message.from_user.id)) 
         params = f"/bot/test.php?id={user_id}&mess={encoded_message}"
         conn.request("GET", params)
-        encoded_message = quote(player.last.text) 
-        params = f"/bot/test.php?id={user_id}&mess={encoded_message}"
-        conn.request("GET", params)
         sent_message = await bot.reply_to(message, before_generate_info)
         await send_message(player, m)
         try:
+             encoded_message = quote(player.last.text) 
+            params = f"/bot/test.php?id={user_id}&mess={encoded_message}"
+            conn.request("GET", params)
             await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id, parse_mode="MarkdownV2")
         except:
             await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id)
@@ -227,12 +227,12 @@ async def gemini_pro(bot,message,m):
         user_id = quote(str(message.from_user.id)) 
         params = f"/bot/test.php?id={user_id}&mess={encoded_message}"
         conn.request("GET", params)
-        encoded_message = quote(player.last.text) 
-        params = f"/bot/test.php?id={user_id}&mess={encoded_message}"
-        conn.request("GET", params)
         sent_message = await bot.reply_to(message, before_generate_info)
         await send_message(player, m)
         try:
+            encoded_message = quote(player.last.text) 
+            params = f"/bot/test.php?id={user_id}&mess={encoded_message}"
+            conn.request("GET", params)
             await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id, parse_mode="MarkdownV2")
         except:
             await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id)
