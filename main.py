3a7d516f21a5fadfad47bdeed73bb4b6eaabcafa
skaz0chn1k <1@mail66.org>
2024-11-01 10:48:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -362,7 +362,7 @@ async def main():
             except Exception:
                 traceback.print_exc()
                 await bot.reply_to(message, error_info)
-            model = genai.GenerativeModel("gemini-pro-vision")
+            model = genai.GenerativeModel("gemini-1.5-pro-002")
             contents = {
                 "parts": [{"mime_type": "image/jpeg", "data": downloaded_file}, {"text": prompt}]
             }
